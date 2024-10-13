99510618555f0128f622cf77e921b71178c97848
Gary <zipe.daden@gmail.com>
2024-10-13 14:56:54 +0800

    修改 project 名稱

--- bestpartner-service/README.md
+++ bestpartner-service/README.md
@@ -3,13 +3,18 @@
 BestPartner project
 ======================
 ## 說明
-1. 該專案為一個AI應用大平台，會結合客製 AI agent 創建工作留
+1. 該專案為一個AI應用大平台，會結合客製 AI agent
 
 ## 事前準備
-安裝 Ollama 並下載 LLM models
+模組平台:
+
+選擇1. 安裝 Ollama 並下載 LLM models
 - Ollama 安裝: [連結](https://blog.darkthread.net/blog/ollam-open-webui/)
 - Docker compose 運行: [連結](https://blog.darkthread.net/blog/ollam-open-webui/)
 
+選擇2. OpenAI 申請 api key [官網](https://openai.com/)
+
+向量資料庫:
 安裝 Chroma
 - Chroma 安裝: [連結](https://cookbook.chromadb.dev/core/install/#chroma-jsts-client)
 
@@ -27,6 +32,7 @@ BestPartner project
 * Langchain4j 0.35.0
 * Gradle latest
 * Postman latest
+* H2 Database latest
 
 ## 備註
 * Langchain4j 官網說明:[連結](https://docs.langchain4j.dev/)
---
+++ bestpartner-service/src/main/kotlin/tw/zipe/basepartner/entity/AigcModel.kt
@@ -0,0 +1,81 @@
+package tw.zipe.basepartner.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+/**
+ * @author Gary
+ * @created 2024/10/12
+ */
+@Entity
+@Table(name = "aigc_model")
+class AigcModel {
+    /**
+     * 主鍵
+     */
+    @Id
+    @Column(name = "id", nullable = false)
+    var id: Long? = null
+
+    /**
+     * 類型
+     */
+    @Column(name = "type", nullable = false)
+    var type: String? = null
+
+    /**
+     * 模型名稱
+     */
+    @Column(name = "model",nullable = true)
+    var model: String? = null
+
+    /**
+     * 供應商
+     */
+    @Column(name = "provider",nullable = true)
+    var provider: String? = null
+
+    /**
+     * 別名
+     */
+    @Column(name = "name",nullable = true)
+    var name: String? = null
+
+    /**
+     * 響應長度
+     */
+    @Column(name = "response_limit",nullable = true)
+    var responseLimit: String? = null
+
+    /**
+     * 溫度
+     */
+    @Column(name = "temperature",nullable = true)
+    var temperature: String? = null
+
+    /**
+     * TOP_P
+     */
+    @Column(name = "top_p",nullable = true)
+    var topP: String? = null
+
+    /**
+     * 供應商 API Key
+     */
+    @Column(name = "api_key",nullable = true)
+    var apiKey: String? = null
+
+    /**
+     * 供應商 URL
+     */
+    @Column(name = "base_url",nullable = true)
+    var baseUrl: String? = null
+
+    /**
+     * 模型維度
+     */
+    @Column(name = "dimension",nullable = true)
+    var dimension: Int? = null
+}
