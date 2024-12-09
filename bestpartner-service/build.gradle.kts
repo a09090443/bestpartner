6ea1370dce115329e63ea655b996e80759266e76
Gary <zipe.daden@gmail.com>
2024-12-09 23:20:51 +0800

    升級 kotlin 版本

--- bestpartner-service/build.gradle.kts
+++ bestpartner-service/build.gradle.kts
@@ -1,9 +1,9 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    kotlin("jvm") version "2.0.10"
-    kotlin("plugin.serialization") version "2.0.10"
-    kotlin("plugin.allopen") version "2.0.10"
+    kotlin("jvm") version "2.1.0"
+    kotlin("plugin.serialization") version "2.1.0"
+    kotlin("plugin.allopen") version "2.1.0"
     id("io.quarkus")
 }
 
