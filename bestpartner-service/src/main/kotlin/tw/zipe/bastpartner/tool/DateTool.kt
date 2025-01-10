0ba6f3edca65272d7637926b3e8d2a79c208c0a7
Gary <zipe.daden@gmail.com>
2025-01-10 23:08:16 +0800

    修改無法取得當地時間問題

--- bestpartner-service/src/main/kotlin/tw/zipe/bastpartner/tool/DateTool.kt
+++ bestpartner-service/src/main/kotlin/tw/zipe/bastpartner/tool/DateTool.kt
@@ -1,6 +1,9 @@
 package tw.zipe.bastpartner.tool
 
+import dev.langchain4j.agent.tool.P
 import dev.langchain4j.agent.tool.Tool
+import java.time.ZoneId
+import java.time.ZonedDateTime
 import tw.zipe.bastpartner.util.time.DateTimeUtils
 
 /**
@@ -10,5 +13,10 @@ import tw.zipe.bastpartner.util.time.DateTimeUtils
 class DateTool {
 
     @Tool("以台灣時間為基準，會根據不同時區取得當地日期時間，格式是yyyy-MM-dd HH:mm:ss")
-    fun getCurrentTime() = DateTimeUtils.getDateNow(DateTimeUtils.dateTimeFormate1)
+    fun getCurrentTime(@P("根據國家或城市回應正確的當地時間") timeZone: String): String {
+        // 直接從指定時區獲取當前時間
+        val zonedDateTime = ZonedDateTime.now(ZoneId.of(timeZone))
+        // 格式化時間
+        return zonedDateTime.format(DateTimeUtils.dateTimeFormate1)
+    }
 }
